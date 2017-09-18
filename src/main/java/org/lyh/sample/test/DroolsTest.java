3e82b44f9c719c426e5011fdf82538db3ff01a41
liuyuhong1981 <liuyuhong1981@gmail.com>
2017-09-18 14:32:07 +0800

    update

--- src/main/java/org/lyh/sample/test/DroolsTest.java
+++ src/main/java/org/lyh/sample/test/DroolsTest.java
@@ -1,5 +1,8 @@
 package org.lyh.sample.test;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.kie.api.KieServices;
 import org.kie.api.runtime.KieContainer;
 import org.kie.api.runtime.KieSession;
@@ -10,6 +13,10 @@ import org.kie.api.runtime.KieSession;
 public class DroolsTest {
 
     public static final void main(String[] args) {
+    	DroolsTest1();
+    }
+
+    public static void DroolsTest1() {
         try {
             // load up the knowledge base
 	        KieServices ks = KieServices.Factory.get();
@@ -27,6 +34,54 @@ public class DroolsTest {
         }
     }
 
+    public static void DroolsTest2() {
+        try {
+            // load up the knowledge base
+	        KieServices ks = KieServices.Factory.get();
+    	    KieContainer kContainer = ks.getKieClasspathContainer();
+        	KieSession kSession = kContainer.newKieSession("ksession-rules");
+
+            // go !
+            Message message = new Message();
+            message.setMessage("Hello World");
+            message.setStatus(Message.HELLO);
+            Owner owner1 = new Owner("aaa", "1");
+            message.setOwner(owner1);
+            List<Owner> owners = new ArrayList<Owner>();
+            Owner owner2 = new Owner("bbb", "2");
+            owners.add(owner1);
+            owners.add(owner2);
+            message.setOwners(owners);
+            kSession.insert(message);
+            kSession.fireAllRules();
+        } catch (Throwable t) {
+            t.printStackTrace();
+        }
+    }
+
+    public static class Owner {
+    	public String name;
+    	public String code;
+    	
+    	public Owner(String name, String code) {
+    		this.name = name;
+    		this.code = code;
+    	}
+
+		public String getName() {
+			return name;
+		}
+		public void setName(String name) {
+			this.name = name;
+		}
+		public String getCode() {
+			return code;
+		}
+		public void setCode(String code) {
+			this.code = code;
+		}
+    }
+
     public static class Message {
 
         public static final int HELLO = 0;
@@ -35,8 +90,30 @@ public class DroolsTest {
         private String message;
 
         private int status;
+        private Owner owner;
+        private List<Owner> owners;
 
-        public String getMessage() {
+        public String hello(String name) {
+        	return "hello " + name;
+        }
+
+        public Owner getOwner() {
+			return owner;
+		}
+
+		public void setOwner(Owner owner) {
+			this.owner = owner;
+		}
+
+		public List<Owner> getOwners() {
+			return owners;
+		}
+
+		public void setOwners(List<Owner> owners) {
+			this.owners = owners;
+		}
+
+		public String getMessage() {
             return this.message;
         }
 
@@ -53,5 +130,4 @@ public class DroolsTest {
         }
 
     }
-
 }
